3266f1f1922c572190f96063a95d9a91f22e715f
Lemon-B-Citrom <lemon.b.citrom@protonmail.com>
2022-03-12 18:42:06 -0500

    Implement writing presets, readme, spawn menu.
    - Write readme to data folder
    - Write presets to data folder
    - Available in spawn menu

--- lua/autorun/client/workshop_collector.lua
+++ lua/autorun/client/workshop_collector.lua
@@ -1,44 +1,44 @@
 local GUI = {}
 local SCRW = ScrW()
 local SCRH = ScrH()
-local FUNCS = {}
-local LAST_WORKSHOP = "https://steamcommunity.com/sharedfiles/filedetails/?id=2457269114"
+local LAST_WORKSHOP = "Collection URL or ID here"
 local DATA_CACHE = {}
 
 --------------------------------------------------------------------------------
 -- Find the ID from a given URL or ID
+--------------------------------------------------------------------------------
 GUI.GetWorkshopID = function(p_string)
     return string.match(p_string, "%d+")
 end
 
-
+--------------------------------------------------------------------------------
 -- Collate data and do things to process them
+-- -TODO- Logic here is %$^@&$% awful
+--------------------------------------------------------------------------------
 GUI.CollateData = function(p_collection_id, p_meta_table, p_content_table)
-    if p_meta_table and p_content_table then
-        local data = {}
-        data.id = p_collection_id
-        data.name = p_meta_table["response"]["publishedfiledetails"][1]["title"]
-        -- Get list of workshop items in collection
-        local content_data = {}
-        if p_content_table.response.collectiondetails[1].children then
-            for _,v in pairs(p_content_table.response.collectiondetails[1].children) do
-                table.insert(content_data, v.publishedfileid)
-            end
-            data.content = content_data
-
-            -- Successful workshop collection operation, save it
-            LAST_WORKSHOP = "https://steamcommunity.com/sharedfiles/filedetails/?id="..p_collection_id
-            GUI.EnableFunc(data)
-        else
-            GUI.DisableFunc(true)
+    local data = {}
+    data.id = p_collection_id
+    data.name = p_meta_table["response"]["publishedfiledetails"][1]["title"]
+    -- Get list of workshop items in collection
+    local content_data = {}
+    if p_content_table.response.collectiondetails[1].children then
+        for _,v in pairs(p_content_table.response.collectiondetails[1].children) do
+            table.insert(content_data, v.publishedfileid)
         end
+        data.content = content_data
+
+        -- Successful workshop collection operation, save it
+        LAST_WORKSHOP = "https://steamcommunity.com/sharedfiles/filedetails/?id="..p_collection_id
+        GUI.EnableSubmitButton(data)
     else
-        GUI.DisableFunc(true)
+        -- Workshop item, but not a collection
+        GUI.DisableSubmitButton(true)
     end
 end
 
 --------------------------------------------------------------------------------
 -- Find the content of a collection ID
+--------------------------------------------------------------------------------
 GUI.QueryCollectionContent = function(p_collection_id, p_meta_table)
     local url = "https://api.steampowered.com/ISteamRemoteStorage/GetCollectionDetails/v1/?"
     local params = {}
@@ -51,19 +51,21 @@ GUI.QueryCollectionContent = function(p_collection_id, p_meta_table)
         if code == 200 then
             GUI.CollateData(p_collection_id, p_meta_table, util.JSONToTable(body))
         else
-            GUI.DisableFunc()
+            GUI.DisableSubmitButton()
             print("Bad collection request for", p_collection_id)
         end
     end,
 
     -- onFailure function
     function(message)
-        GUI.DisableFunc()
+        print("Failed collection content request OUTRIGHT for", p_collection_id)
+        GUI.DisableSubmitButton()
     end)
 end
 
 --------------------------------------------------------------------------------
 -- Find the metadata of a collection ID
+--------------------------------------------------------------------------------
 GUI.QueryCollectionMetadata = function(p_collection_id)
     local url = "https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/?"
     local params = {} 
@@ -76,26 +78,29 @@ GUI.QueryCollectionMetadata = function(p_collection_id)
         if code == 200 then
             GUI.QueryCollectionContent(p_collection_id, util.JSONToTable(body))
         else
-            GUI.DisableFunc()
+            GUI.DisableSubmitButton()
             print("Bad metadata request for", p_collection_id)
         end
     end,
 
     -- onFailure function
     function(message)
-        GUI.DisableFunc()
+        print("Failed collection metadata request OUTRIGHT for", p_collection_id)
+        GUI.DisableSubmitButton()
     end)
 end
 
 
 --------------------------------------------------------------------------------
 -- Call both REST queries implicitly
+--------------------------------------------------------------------------------
 GUI.GetWorkshopInfo = function(p_collection_id)
     GUI.QueryCollectionMetadata(p_collection_id)
 end
 
 --------------------------------------------------------------------------------
--- Populate Table
+-- Populate Table with IDs and names
+--------------------------------------------------------------------------------
 GUI.PopulateItemIDs = function(p_item_list)
     local url = "https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/?"
     local params = {} 
@@ -119,7 +124,7 @@ GUI.PopulateItemIDs = function(p_item_list)
                 GUI.Display:AddLine(v.publishedfileid, v.title or "***Deleted Item***")
             end
         else
-            GUI.DisableFunc()
+            GUI.DisableSubmitButton()
             print("Bad metadata request for workshop item", p_item_id)
         end
     end,
@@ -132,11 +137,15 @@ end
 
 --------------------------------------------------------------------------------
 -- Enable Functionality
-GUI.EnableFunc = function(p_data)
+-- Populates just IDs, initially
+--------------------------------------------------------------------------------
+GUI.EnableSubmitButton = function(p_data)
     DATA_CACHE = p_data
     GUI.Submit:SetEnabled(true)
     -- -TODO- make green
     GUI.Submit:ColorTo(Color(50,50,200), 1, 0)
+    GUI.DispLabel:SetEnabled(false)
+    GUI.DispLabel:SetText(p_data.name)
     GUI.Display:SetEnabled(true)
     GUI.Display:Clear()
     for _,v in pairs(p_data.content) do
@@ -147,19 +156,23 @@ end
 
 --------------------------------------------------------------------------------
 -- Disable Functionality
-GUI.DisableFunc = function(p_visible_error)
+--------------------------------------------------------------------------------
+GUI.DisableSubmitButton = function(p_visible_error)
     p_visible_error = p_visible_error or false
     GUI.Submit:SetEnabled(false)
     GUI.Submit:ColorTo(Color(255,255,255), 1, 0)
     GUI.Display:SetEnabled(false)
+    GUI.DispLabel:SetEnabled(false)
+    GUI.DispLabel:SetText("Collection")
     GUI.Display:Clear()
     if p_visible_error then
-        Derma_Message("Supplied item is probably not a Garry's Mod Collection...")
+        Derma_Message("Supplied item is probably not a Garry's Mod Collection...", "Error")
     end
 end
 
 --------------------------------------------------------------------------------
 -- Load Menu
+--------------------------------------------------------------------------------
 local function WorkshopCollectorMenu()
     
     -- Frame
@@ -196,8 +209,13 @@ local function WorkshopCollectorMenu()
     GUI.Submit:SetEnabled(false)
 
     -- Display
+    GUI.DispLabel = vgui.Create("DLabel", GUI.Frame)
+    GUI.DispLabel:SetPos(10, 90)
+    GUI.DispLabel:SetSize(frame_width - 20, 20)
+    GUI.DispLabel:SetText("Collection")
+    GUI.DispLabel:SetEnabled(false)
     GUI.Display = vgui.Create("DListView", GUI.Frame)
-    GUI.Display:SetPos(10, 90)
+    GUI.Display:SetPos(10, 110)
     GUI.Display:SetSize(frame_width - 20, (frame_height - 120))
     GUI.Display:SetMultiSelect(false)
     GUI.Display:AddColumn("ID"):SetMaxWidth(100)
@@ -210,30 +228,80 @@ local function WorkshopCollectorMenu()
         GUI.GetWorkshopInfo(onload_id)
     end
 
+    -- Select Collection
     GUI.Query.DoClick = function()
         -- Get ID or exit
         local id = GUI.GetWorkshopID(GUI.Entry:GetText())
         if not id then
-            GUI.Submit:SetEnabled(false)
-            Derma_Message("Please supply a valid workshop collection URL or ID")
+            GUI.DisableSubmitButton()
+            Derma_Message("Please supply a valid workshop collection URL or ID", "Error")
             return
         end
+        GUI.DisableSubmitButton()
         GUI.GetWorkshopInfo(id)
     end
-
+    
+    -- Submit Collection
     GUI.Submit.DoClick = function()
-        presets = util.JSONToTable(LoadAddonPresets())
-        presets[DATA_CACHE.name] = {}
+        local preset_name = DATA_CACHE.name.." ("..DATA_CACHE.id..")"
+        presets = util.JSONToTable(file.Read("settings/addonpresets.txt", true))
+        presets[preset_name] = {}
         
-        target_preset = presets[DATA_CACHE.name]
+        target_preset = presets[preset_name]
         target_preset.disabled = {}
         target_preset.enabled = DATA_CACHE.content
         target_preset.name = DATA_CACHE.name
         target_preset.newAction = ""
         
-        SaveAddonPresets(util.TableToJSON(target_preset))
+        GUI.CreateReadMeAndDir()
+        file.Write("workshop_collector/addonpresets.txt", util.TableToJSON(presets))
+        Derma_Message("Preset \""..preset_name.."\" created in GarrysMod\\garrysmod\\data\\workshop_collector", "Success")
+    end
+
+    -- View workshop item
+    -- -TODO- Why does this syntax work but not
+    -- GUI.Display.DoDoubleClick = function(lineID, line)
+    function GUI.Display:DoDoubleClick(lineID, line)
+        local workshop_id = line:GetColumnText(1)
+        gui.OpenURL("https://steamcommunity.com/sharedfiles/filedetails/?id="..workshop_id)
     end
 end
 
 --------------------------------------------------------------------------------
+-- Tool menu
+--------------------------------------------------------------------------------
 concommand.Add("workshop_collector_menu", WorkshopCollectorMenu)
+hook.Add("PopulateToolMenu", "WorkshopCollectorPopulateToolMenu", function()
+        spawnmenu.AddToolMenuOption(
+                "Utilities", "User", "WorkshopCollectorToolMenu", 
+                "Open Workshop Collector", "workshop_collector_menu")
+    end)
+
+--------------------------------------------------------------------------------
+-- Create readme file
+--------------------------------------------------------------------------------
+GUI.CreateReadMeAndDir = function()
+    text = [[
+### README ###
+
+How to Install:
+---------------
+1. Close Garry's Mod
+2. Navigate to GarrysMod\garrysmod\settings
+3. Rename addonpresets.txt into addonpresets_bak.txt
+4. Copy the addonpresets.txt in this data folder to GarrysMod\garrysmod\settings
+5. Start Garry's Mod
+6. Your preset should now be listed and selectable
+
+"Un-installation" if your presets are broken:
+------------------
+1. Close Garry's Mod
+2. Navigate to GarrysMod\garrysmod\settings
+3. Delete addonpresets.txt into 
+4. Rename addonpresets_bak.txt to addonpresets.txt
+5. Start Garry's Mod
+6. Things should be how you left them
+]]
+    file.CreateDir("workshop_collector")
+    file.Write("workshop_collector/README.txt", text)
+end
