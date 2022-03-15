03e1bed86079c7c4553087115f6cd81366029be0
Lemon-B-Citrom <lemon.b.citrom@protonmail.com>
2022-03-14 23:44:24 -0400

    /me dodges the gmod blacklist

--- lua/menu/README.lua
+++ lua/menu/README.lua
@@ -1,3 +1,4 @@
+--[[
 Files in this directly need to manually be copied into GarrysMod\garrysmod\lua\menu
 
 If you have other menu mods, such as Addon managers, you may need to edit your own menu.lua manually. After copying workshop_collector_menu.lua, simply add...
@@ -5,3 +6,4 @@ If you have other menu mods, such as Addon managers, you may need to edit your o
     include( "workshop_collector_menu.lua" ) -- Workshop Collector
 
 ...inside of your menu.lua
+]]--
